9c5478fc801ab422e5f57a440c4329a97c7ba95d
ammara <hoosenammara@gmail.com>
2025-04-28 18:58:13 +0200

    final updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import Footer from './components/Footer';
 import BackToTopButton from './components/BackToTopButton';
 
 export const metadata = {
-  title: 'My Portfolio',
-  description: 'Created with Next.js',
+  title: 'Ammara Hoosen | Web Developer Portfolio | Cape Town',
+  description: 'Explore the creative portfolio of Ammara, a frontend web developer passionate about building beautiful, responsive websites. Available for freelance projects.',
 };
 
 export default function RootLayout({ children }) {
